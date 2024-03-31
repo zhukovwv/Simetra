c5197eeeff06a5825e27d479b934f66b0d7ede5b
Georgy Zhukov <g.zhukov@ripe.capital>
2024-03-31 03:37:42 +0300

    Import fixe

--- src/parse_gps_excel_data.py
+++ src/parse_gps_excel_data.py
@@ -2,7 +2,7 @@ import asyncio
 from services import load_data_from_excel
 
 # Путь к файлу Excel с данными GPS
-file_path = "src/data/2_5420464171701519891.xlsx"
+file_path = "data/2_5420464171701519891.xlsx"
 
 
 # Запускаем функцию загрузки данных из Excel в базу данных
